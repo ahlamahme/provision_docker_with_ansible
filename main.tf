b8d008e7aa222bf7c62306201ba04d80c201f673
ahlam <ahlamahmed21101998@gmail.com>
2023-05-17 19:00:08 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,60 @@
+locals {
+  vpc_id           = "vpc-09d0cfd43a7376dc9"
+  subnet_id        = "subnet-0ebeef43ad4830ad6"
+  ssh_user         = "ubuntu"
+  key_name         = "ansible"
+  private_key_path = "/home/ahlam/Downloads/ansible.pem"
+}
+
+resource "aws_security_group" "nginx" {
+  name   = "nginx_access"
+  vpc_id = local.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami                         = "ami-0aa2b7722dc1b5612"
+  subnet_id                   = "subnet-0ebeef43ad4830ad6"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.nginx.id]
+  key_name                    = local.key_name
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = file(local.private_key_path)
+      host        = aws_instance.nginx.public_ip
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook  -i ${aws_instance.nginx.public_ip}, --private-key ${local.private_key_path} docker2.yaml -u ubuntu "
+  }
+}
+
+output "nginx_ip" {
+  value = aws_instance.nginx.public_ip
+}
